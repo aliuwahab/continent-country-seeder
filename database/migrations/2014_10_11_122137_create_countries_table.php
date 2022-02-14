cff78e08727f4604e537d4f947061732c8a6b367
Aliu <aliuwahab@gmail.com>
2022-02-14 12:53:02 +0100

    Categorize countries into continents and add currency codes

--- database/migrations/2014_10_11_122137_create_countries_table.php
+++ database/migrations/2014_10_11_122137_create_countries_table.php
@@ -15,9 +15,9 @@ return new class extends Migration {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->foreignId('continent_id')->constrained('continents')->cascadeOnDelete();
-            $table->string('name');
-            $table->string('iso2_code');
-            $table->string('iso3_code');
+            $table->string('name')->unique();
+            $table->string('iso2_code')->unique();
+            $table->string('iso3_code')->unique();
             $table->string('currency')->default('USD');
         });
     }
