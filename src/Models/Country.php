0aebac8a6b3f89e504ae172da6caea76f90a6b9b
Tjardo <dev@tjardo.com>
2022-02-15 13:52:48 +0100

    Set dynamic table name on models

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -10,6 +10,13 @@ class Country extends Model
 {
     protected $guarded = ['id'];
 
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->setTable(config('continent-country-seeder.countries_table_name'));
+    }
+
     public function continent(): BelongsTo
     {
         return $this->belongsTo(Continent::class);
